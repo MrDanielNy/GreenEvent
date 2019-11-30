225b311e59bcd3f651b632f9d17d52e55800bc5b
MrDanielNy <tjanster1@danielny.se>
2019-11-30 09:18:32 +0100

    Fixed everything for new start
    Fixed everything for new start

--- Program.cs
+++ Program.cs
@@ -4,14 +4,8 @@ namespace GreenEvent
 {
     class Program
     {
-
-
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
-
-            //DataBase.CreateRolesForDebug();
-            //DataBase.CreateUsersForDebug();
-
             EventSystem greenEvent = new EventSystem();
 
             greenEvent.Start();
